527df91a19c5054b195b97fe78bb8c3cb880048c
Evgeniy16312 <turin.92@mail.ru>
2022-06-09 21:29:06 +0300

    создан класс toolbarApp в папке utils с заложенным запасом функционала для будующего. Тулбары изменены во всём приложении.

--- app/src/main/java/ru/edamamlearning/graduationproject/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/edamamlearning/graduationproject/ui/search/SearchFragment.kt
@@ -3,8 +3,10 @@ package ru.edamamlearning.graduationproject.ui.search
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
@@ -21,6 +23,7 @@ import ru.edamamlearning.graduationproject.core.viewBinding
 import ru.edamamlearning.graduationproject.databinding.FragmentSearchBinding
 import ru.edamamlearning.graduationproject.di.viewmodelsfactory.ViewModelFactory
 import ru.edamamlearning.graduationproject.domain.model.FoodDomainModel
+import ru.edamamlearning.graduationproject.utils.ToolbarApp
 import ru.edamamlearning.graduationproject.utils.hideKeyboard
 import ru.edamamlearning.graduationproject.utils.saveNavigate
 import javax.inject.Inject
@@ -44,7 +47,7 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
             favouriteClickHandler = viewModel::favouriteFoodClickHandler,
             diaryClickHandler = viewModel::diaryFoodClickHandler,
 
-        )
+            )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -70,7 +73,7 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
 
     private fun navigate(foodDomainModel: FoodDomainModel) {
         val action = SearchFragmentDirections
-           .actionSearchFragmentToInfoFragment(foodDomainModel.foodId)
+            .actionSearchFragmentToInfoFragment(foodDomainModel.foodId)
         findNavController().navigate(action)
     }
 
@@ -114,6 +117,29 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
 
     private fun toggleLoader(loading: Boolean) {
         binding.progressBar.isVisible = loading
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        val toolbar = ToolbarApp()
+
+        toolbar.setToolBar(
+            activity = requireActivity(),
+            title = "Поиск",
+            visibleRight = true,
+            visibleLeft = false
+        )
+
+        (requireActivity().findViewById<AppCompatImageView>(R.id.info)).setImageResource(R.drawable.ic_info)
+        (requireActivity().findViewById<AppCompatImageView>(R.id.back)).setImageResource(R.drawable.ic_toolbar_back_button)
+
+        (requireActivity().findViewById<View>(R.id.info) as ImageView).setOnClickListener {
+            Toast.makeText(context, "info", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
