b407c7ffd398b211522d8d13e5878abd86611044
kosykt <70757252+kosykt@users.noreply.github.com>
2022-06-12 14:52:38 +0900

    set ActionBar for AppTheme

--- app/src/main/java/ru/edamamlearning/graduationproject/ui/MainActivity.kt
+++ app/src/main/java/ru/edamamlearning/graduationproject/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
@@ -39,9 +40,21 @@ class MainActivity : DaggerActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         configureNavigation()
         supportFragmentManager.beginTransaction()
         subscribeOnSystemMessages()
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onStart() {
@@ -62,9 +75,6 @@ class MainActivity : DaggerActivity(R.layout.activity_main) {
     }
 
     private fun configureNavigation() {
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-        navController = host.navController
         bottomNavigationView = findViewById(R.id.bottom_navigation_view)
         bottomNavigationView.setupWithNavController(navController)
 
